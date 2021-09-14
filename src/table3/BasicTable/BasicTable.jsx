c925ba2b8b436fc5e9f35de574a1514718f96d6b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-14 23:14:29 +0300

    moving tables here

---
+++ src/table3/BasicTable/BasicTable.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Table from '../Table/Table.jsx'
+
+// TODO: add styles as props from the outside
+const BasicTable = ({children}) => (
+
+  <Table 
+    border="0" 
+    cellpadding="0" 
+    cellspacing="0" 
+    role="presentation" 
+    width="100%">
+    <tbody>
+
+      {children}
+
+    </tbody>
+  </Table>
+
+);
+
+export default BasicTable;
