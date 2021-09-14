87b24e4db64972a395a14e4be4807f88621908d0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-14 21:02:05 +0000

    upd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@
 import Table from './Table/Table.jsx';
 import TableLeft from './TableLeft/tableLeft.jsx';
 
+// import RowOne from './RowOne/RowOne.jsx'
+import BasicTable from './BasicTable/BasicTable.jsx';
+
 export {
     Table,
-    TableLeft
+    TableLeft,
+    BasicTable
 };
